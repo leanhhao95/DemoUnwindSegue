f0e5b954aef4f6b18344555e34d0997ae5e80c4f
Hao <lehuongchu@gmail.com>
2017-10-16 07:42:54 +0700

    update

--- DemoUnWindSegue/ViewController.swift
+++ DemoUnWindSegue/ViewController.swift
@@ -5,21 +5,35 @@
 //  Created by Hao on 31/08/2017.
 //  Copyright © 2017 Hao. All rights reserved.
 //
-
+import os.log
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var dataTextField: UITextField!
+    
+    var displayInt : Int?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        if displayInt != 0 {
+           dataTextField.text = ("\(displayInt!)")
+        }
+           }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        guard let button = sender as? UIBarButtonItem , button == saveButton else {
+            os_log("The save button was not pressed, cancelling", log: OSLog.default, type: .debug)
+            return
+        }
+        displayInt = Int(dataTextField.text!)
+        }
+        
+        }
 
-}
 
